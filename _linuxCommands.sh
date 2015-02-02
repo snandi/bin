5057957819e6b791d9d58ce70dfd8b2a4d0b08a0
snandi <nandi@stat.wisc.edu>
2015-02-01 19:58:48 -0600

    Added awk commands to linuxcommands

--- _linuxCommands.sh
+++ _linuxCommands.sh
@@ -1,11 +1,11 @@
-1. To change the prompt on the shell, to display the current directory, do:
+** To change the prompt on the shell, to display the current directory, do:
    $ PS1='\h:$( pwd )\$ '
    It will only change for the current terminal, as the .bashrc and .bash_profile files are not writable.
 
-2. To merge 2 pdf files and create a new one:
+** To merge 2 pdf files and create a new one:
    pdftk File1.pdf File2.pdf cat output OutputFile.pdf
 
-3. To rotate the entire pdf file 90 degrees clockwise:
+** To rotate the entire pdf file 90 degrees clockwise:
    pdftk Input.pdf cat 1-endE output Output.pdf
    
    To rotate the entire pdf file 180 degrees clockwise:
@@ -24,7 +24,7 @@
    pdftk File1 cat 1-2 output Temp1; pdftk File2 cat 3-4 output Temp2; pdftk Temp1 File2 Temp2 cat output FinalOutput.pdf; rm Temp*
    
 #######################################################################################################
-4. GIT commands
+** GIT commands
    (a) Add a connection to your friend’s version of the github repository, if you haven’t already:
        Go to ~/allez/, then type
        git remote add atbroman git://github.com/atbroman/allez
@@ -50,103 +50,114 @@
 
 #######################################################################################################
 
-5. To spell check in a terminal:
+** To spell check in a terminal:
    aspell -t -c LitReview.tex
 
-6. To change R/W permissions for a folder, its subfolders and all their files, do:
+** To change R/W permissions for a folder, its subfolders and all their files, do:
    chmod 700 -R folder
 
-7. To print first column (or any column) of a text file, using awk:
+############################################ awk commands #############################################
+** To print first column (or any column) of a text file, using awk:
    awk '{print $1}' filename
 
    To print columns 1 and 4 of a text file with a space between them, using awk:
    awk '{print $1 " " $4}' filename > output
 
-8. To delete rows from a file satisfying a particular pattern, for example "NANDI"
+   To search for a pattern in column 1 and print the whole row for those successful searches
+   awk '$1 == "pattern" {print $0}' filename
+
+   To search for a pattern in column 1 and print some of the columns for those successful searches
+   awk '$1 == "pattern" {print $1 " " $4}' filename
+
+#######################################################################################################
+
+############################################ sed commands #############################################
+** To delete rows from a file satisfying a particular pattern, for example "NANDI"
    sed '/NANDI/d' filename.txt
 
-9. To grep multiple strings in a file:
+#######################################################################################################
+
+** To grep multiple strings in a file:
    grep 'String1\|String2\|String3' filename
 
-10. To check which users have access to a folder on AFS:
-    fs listacl . or fs listacl /folderpath
+** To check which users have access to a folder on AFS:
+   fs listacl . or fs listacl /folderpath
 
-11. To provide access to a particular folder on AFS:
-    fs setacl -dir /afs/cs.wisc.edu/u/n/a/nandi/Stat/Stat_Quals/ -acl huling rl
+** To provide access to a particular folder on AFS:
+   fs setacl -dir /afs/cs.wisc.edu/u/n/a/nandi/Stat/Stat_Quals/ -acl huling rl
 
-12. To convert a jpg file to pdf:
-    convert file.jpg file.pdf
+** To convert a jpg file to pdf:
+   convert file.jpg file.pdf
 
-13. To sort the folders/files in a directory by their sizes:
-    du -hs * | sort -h
+** To sort the folders/files in a directory by their sizes:
+   du -hs * | sort -h
 
-14. To sync local and remote folder contents:
-    rsync -av nandi@desk00.stat.wisc.edu:Courses/BMI576_Fall2014/ BMI576_Fall2014  ## To sync contents from Stat folder to LMCG folder
-    rsync -av BMI576_Fall2014/ nandi@desk00.stat.wisc.edu:Courses/BMI576_Fall2014  ## To sync contents from LMCG folder to Stat folder
+** To sync local and remote folder contents:
+   rsync -av nandi@desk00.stat.wisc.edu:Courses/BMI576_Fall2014/ BMI576_Fall2014  ## To sync contents from Stat folder to LMCG folder
+   rsync -av BMI576_Fall2014/ nandi@desk00.stat.wisc.edu:Courses/BMI576_Fall2014  ## To sync contents from LMCG folder to Stat folder
 
-15. To count the number of words in a pdf document:
-    pdftotext document.pdf -enc UTF-8 - | wc -m
+** To count the number of words in a pdf document:
+   pdftotext document.pdf -enc UTF-8 - | wc -m
 
-16. To check if a linux computer is 32 bit or 64 bit:
-    a) file /sbin/init
-    b) uname -a 
-    To check the number of processors:
-    a) cat /proc/cpu
+** To check if a linux computer is 32 bit or 64 bit:
+   a) file /sbin/init
+   b) uname -a 
+   To check the number of processors:
+   a) cat /proc/cpu
 
-17. To install a .deb package on ubuntu:
-    sudo dpkg -i DEB_PACKAGE
-    For example if the package file is called askubuntu_2.0.deb then you should do 
-    sudo dpkg -i askubuntu_2.0.deb. 
-    If dpkg reports an error due to dependency problems, you can run 
-    sudo apt-get install -f 
-    to download the missing dependencies and configure everything. 
-    
-    REMOVE A PACKAGE
-    sudo dpkg -r PACKAGE_NAME
+** To install a .deb package on ubuntu:
+   sudo dpkg -i DEB_PACKAGE
+   For example if the package file is called askubuntu_2.0.deb then you should do 
+   sudo dpkg -i askubuntu_2.0.deb. 
+   If dpkg reports an error due to dependency problems, you can run 
+   sudo apt-get install -f 
+   to download the missing dependencies and configure everything. 
+   
+   REMOVE A PACKAGE
+   sudo dpkg -r PACKAGE_NAME
 
-18. synergy configuration file
-    #####################################
-    section: screens
-    boltzmann:
-    doty:
-    end
+** Synergy configuration file
+   #####################################
+   section: screens
+   boltzmann:
+   doty:
+   end
 
-    section: aliases
-    doty:
-    192.168.0.101
-    end
+   section: aliases
+   doty:
+   192.168.0.101
+   end
 
-    section: links
-    boltzmann:
-    left = doty
-    doty:
-    right = boltzmann
-    end
+   section: links
+   boltzmann:
+   left = doty
+   doty:
+   right = boltzmann
+   end
 
-    section: options
-    screenSaverSync = false
-    end
-    #####################################
+   section: options
+   screenSaverSync = false
+   end
+   #####################################
 
-19. To run R programs from command line:
-    R CMD BATCH RScript.R > RScript.Rout # The .Rout is optional.
+** To run R programs from command line:
+   R CMD BATCH RScript.R > RScript.Rout # The .Rout is optional.
 
-    To run it remotely from and continue running it even after logging out of server:
-    nohup R CMD BATCH RScript.R &
-    Then type "exit" to exit the terminal
+   To run it remotely from and continue running it even after logging out of server:
+   nohup R CMD BATCH RScript.R &
+   Then type "exit" to exit the terminal
 
-20. To print only unique rows from one file into another:
-    sort -u file1 > file2
+** To print only unique rows from one file into another:
+   sort -u file1 > file2
 
-21. To see full paths of commands in top:
-    top -c -u nandi
+** To see full paths of commands in top:
+   top -c -u nandi
 
-22. To list only empty subdirectories in a particular directory:
-    find . -type d -empty
+** To list only empty subdirectories in a particular directory:
+   find . -type d -empty
 
-23. To dynamically watch a file/folder being populated by some script:
-    watch ls -lht             # watch the folder
-    tail -f filename          # dynamically watch the file being populated
-    less +F filename          # dynamically watch the file being populated
-
+** To dynamically watch a file/folder being populated by some script:
+   watch ls -lht             # watch the folder
+   tail -f filename          # dynamically watch the file being populated
+   less +F filename          # dynamically watch the file being populated
 
