4ba2544450a397388a1aa7fc02f13eecdcd45ad6
snandi <nandi@stat.wisc.edu>
2015-02-11 00:59:25 -0600

    Added scp/ssh capability without passwd prompting

--- _linuxCommands.sh
+++ _linuxCommands.sh
@@ -141,7 +141,7 @@
    section: options
    screenSaverSync = false
    end
-   #####################################
+########################################
 
 *] To run R programs from command line:
    R CMD BATCH RScript.R > RScript.Rout # The .Rout is optional.
@@ -164,3 +164,19 @@
    tail -f filename          # dynamically watch the file being populated
    less +F filename          # dynamically watch the file being populated
 
+########################################
+*] To ssh/rcp/rsync without password prompting:
+
+   Lets say you want to copy between two hosts host_A and host_B. host_A is the host where you would run the scp, ssh or rsyn command, irrespective of the direction of the file copy! On host_A, run this command as the user that runs scp/ssh/rsync
+
+   ssh-keygen -t rsa
+
+   This will prompt for a passphrase. Just press the enter key. It will then generate an identification (private key) and a public key. Do not ever share the private key with anyone! ssh-keygen shows where it saved the public key. This is by default ~/.ssh/id_rsa.pub: Your public key has been saved in <your_home_dir>/.ssh/id_rsa.pub
+   
+   Transfer the id_rsa.pub file to host_B by either ftp, scp, rsync or any other method. 
+   On host_B, login as the remote user which you plan to use when you run scp, ssh or rsync on host_src. Copy the contents of id_rsa.pub from host_A to ~/.ssh/authorized_keys on host_B 
+   chmod 700 ~/.ssh/authorized_keys
+
+   All set!
+
+########################################
